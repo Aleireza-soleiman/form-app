8bacb0f25a3d8d25795ad1afa931163949ddf533
Alireza Soleiman <alireza.soleiman9719@gmail.com>
2021-10-12 18:37:23 +0330

    using validate function

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,31 +1,40 @@
-import React , { useState } from 'react';
+import React , { useState , useEffect } from 'react';
 import { Link } from 'react-router-dom';
-
+//function
+import { validate } from '../functions/validate';
 const Login = () => {
-    const [touched , setTouched] = useState({})
+    const [touched , setTouched] = useState({});
+    const [errors , setErrors] = useState({});
     const [data , setData] = useState({
         email : "" ,
         password : "" 
     });
+
+    useEffect(()=>{
+        setErrors(validate(data , "login"))
+    },[data , touched]);
+
     const changeHandler = event =>{
         setData({...data , [event.target.name] : event.target.value})
     }
     const focusHandler = event =>{
         setTouched({...touched , [event.target.name] : true})
     }
-
+    const submitHandler = event => event.preventDefault();
 
     return (
         <div>
-            <form>
+            <form onSubmit={submitHandler} >
                 <h2>Login</h2>
                 <div>
                     <label>Email</label>
                     <input type="text" name="email" value={data.email} onChange={changeHandler} onFocus={focusHandler} />
+                    {errors.email && touched.email && <span>{errors.email}</span>}
                 </div>
                 <div>
                     <label>Password</label>
                     <input type="text" name="password" value={data.password} onChange={changeHandler} onFocus={focusHandler} />
+                    {errors.password && touched.password && <span>{errors.password}</span>}
                 </div>
                 <div>
                     <Link to="/signup" >SignUp</Link>
--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,15 +1,22 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
-
+//function
+import { validate } from '../functions/validate';
 const SignUp = () => {
     const [touched , setTouched] = useState({})
+    const [errors , setErrors] = useState({});
     const [data , setData] = useState({
         name : "" ,
         email : "" ,
         password : "" ,
         confirmPassword : "" ,
-        isAccepted : true 
+        isAccepted : false 
     });
+
+    useEffect(()=>{
+        setErrors(validate(data , "signup"))
+    },[data , touched]);
+
     const changeHandler = event =>{
         if(event.target.name === "isAccepted"){
             setData({...data , [event.target.name] : event.target.checked })
@@ -21,30 +28,37 @@ const SignUp = () => {
         setTouched({...touched , [event.target.name] : true})
     }
 
+    const submitHandler = event => event.preventDefault();
+
     return (
         <div>
-            <form>
+            <form onSubmit={submitHandler} >
                 <h2>SignUp</h2>
                 <div>
                     <label>Name</label>
                     <input type="text" name="name" value={data.name} onChange={changeHandler} onFocus={focusHandler} />
+                    {errors.name && touched.name && <span>{errors.name}</span>}
                 </div>
                 <div>
                     <label>Email</label>
                     <input type="text" name="email" value={data.email} onChange={changeHandler} onFocus={focusHandler} />
+                    {errors.email && touched.email && <span>{errors.email}</span>}
                 </div>
                 <div>
                     <label>Password</label>
                     <input type="text" name="password" value={data.password} onChange={changeHandler} onFocus={focusHandler} />
+                    {errors.password && touched.password && <span>{errors.password}</span>}
                 </div>
                 <div>
                     <label>ConfirmPassword</label>
                     <input type="text" name="confirmPassword" value={data.confirmPassword} onChange={changeHandler} onFocus={focusHandler} />
+                    {errors.confirmPassword && touched.confirmPassword && <span>{errors.confirmPassword}</span>}
                 </div>
                 <div>
                     <div>
                         <label>I accept terms of privacy policy</label>
                         <input type="checkbox" name="isAccepted" value={data.isAccepted} onChange={changeHandler} onFocus={focusHandler} />
+                        {errors.isAccepted && touched.isAccepted && <span>{errors.isAccepted}</span>}
                     </div>
                 </div>
                 <div>
