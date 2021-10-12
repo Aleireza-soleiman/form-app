582b8624b499d799a9e720d4b803d0ea77599926
Alireza Soleiman <alireza.soleiman9719@gmail.com>
2021-10-12 17:48:48 +0330

    import components to app.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-
+import { Redirect, Route, Switch } from 'react-router-dom';
 import './App.css';
+//components
+import Login from './components/Login';
+import SignUp from './components/SignUp';
 
 function App() {
   return (
     <div className="App">
-    
+      <Switch>
+        <Route path="/login" component={Login} />
+        <Route path="/signup" component={SignUp} />
+        <Redirect from="/" to="/signup" />
+      </Switch>
     </div>
   );
 }
